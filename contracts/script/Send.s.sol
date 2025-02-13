5284e3374c4d440277775e1196f30bec56db236f
Guilherme Dantas <guidanoli@proton.me>
2025-02-13 16:16:56 -0300

    Encode role as str

--- contracts/script/Send.s.sol
+++ contracts/script/Send.s.sol
@@ -6,78 +6,9 @@ import {Script} from "forge-std/Script.sol";
 
 import {Completer} from "../src/Completer.sol";
 import {Callback} from "../src/Callback.sol";
-import {Role, Message, Option, Request} from "../src/Types.sol";
-
-struct RawMessage {
-    string content;
-    string role;
-}
-
-struct RawRequest {
-    uint256 maxCompletionTokens;
-    RawMessage[] messages;
-    string model;
-    Option[] options;
-}
-
-library LibString {
-    using LibString for string;
-
-    error UnknownRoleString(string str);
-
-    function toRole(string memory str) internal pure returns (Role) {
-        if (str.eq("system")) {
-            return Role.SYSTEM;
-        } else if (str.eq("assistant")) {
-            return Role.ASSISTANT;
-        } else if (str.eq("user")) {
-            return Role.USER;
-        } else {
-            revert UnknownRoleString(str);
-        }
-    }
-
-    function eq(string memory str1, string memory str2) internal pure returns (bool) {
-        return str1.hash() == str2.hash();
-    }
-
-    function hash(string memory str) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked(str));
-    }
-}
-
-library LibRawMessage {
-    using LibString for string;
-    using LibRawMessage for RawMessage;
-
-    function convert(RawMessage memory rawMessage) internal pure returns (Message memory) {
-        return Message({role: rawMessage.role.toRole(), content: rawMessage.content});
-    }
-
-    function convert(RawMessage[] memory rawMessages) internal pure returns (Message[] memory messages) {
-        messages = new Message[](rawMessages.length);
-        for (uint256 i; i < rawMessages.length; ++i) {
-            messages[i] = rawMessages[i].convert();
-        }
-    }
-}
-
-library LibRawRequest {
-    using LibRawMessage for RawMessage[];
-
-    function convert(RawRequest memory rawRequest) internal pure returns (Request memory) {
-        return Request({
-            modelName: rawRequest.model,
-            maxCompletionTokens: rawRequest.maxCompletionTokens,
-            messages: rawRequest.messages.convert(),
-            options: rawRequest.options
-        });
-    }
-}
+import {Message, Option, Request} from "../src/Types.sol";
 
 contract SendScript is Script {
-    using LibRawRequest for RawRequest;
-
     function send(
         Completer completer,
         Callback callback,
@@ -94,8 +25,7 @@ contract SendScript is Script {
 
     function _loadRequestFromJsonFile(string calldata requestJsonPath) internal view returns (Request memory) {
         string memory requestJson = vm.readFile(requestJsonPath);
-        bytes memory encodedRawRequest = vm.parseJson(requestJson);
-        RawRequest memory rawRequest = abi.decode(encodedRawRequest, (RawRequest));
-        return rawRequest.convert();
+        bytes memory encodedRequest = vm.parseJson(requestJson);
+        return abi.decode(encodedRequest, (Request));
     }
 }
--- getresult.sh
+++ getresult.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 completion_id=$1
 shift 1
 SimpleCallback=$(cat contracts/deployments/SimpleCallback)
-ResultReceived='ResultReceived(uint256 indexed,(uint8,string)[],(uint256,uint256))'
+ResultReceived='ResultReceived(uint256 indexed,(string,string)[],(uint256,uint256))'
 logs=$(cast logs "$@" --json --address "$SimpleCallback" "$(cast sig-event "$ResultReceived")" "$(cast to-uint256 "$completion_id")")
 nlogs=$(echo "$logs" | jq length)
 [ $nlogs -ge 1 ] || (>&2 echo "No result for completion #$completion_id yet." && exit 1)
